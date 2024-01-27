40071146cd136aad5d8b86e946daa62a6a2d7eb3
Leo Theobald <jonathan.theobald@futurenet.com>
2024-01-27 14:15:22 +0000

    small refactor + mixins btn

--- src/index.js
+++ src/index.js
@@ -4,3 +4,24 @@ import './site.scss';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+// const observer = new IntersectionObserver((sections) => {
+//   sections.forEach((section) => {
+//     if (section.isIntersecting) {
+//       section.target.classList.add('open');
+//     } else {
+//       section.target.classList.remove('open');
+//     }
+//   });
+// });
+
+// const sectionElements = document.querySelectorAll('section');
+// const closedElements = document.querySelectorAll('.closed');
+// const buttonIdClick = document.querySelectorAll('a');
+
+// for (let i = 0; i < buttonIdClick.length; i++) {
+//   sectionElements[i].setAttribute('id', + i);
+//   buttonIdClick[i].setAttribute('href', `#${i+1}`);
+// }
+
+// closedElements.forEach((el) => observer.observe(el));
